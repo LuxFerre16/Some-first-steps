c57e8b9b67e10472d9b25d45013055ada9fb17aa
Khalid <khaimqse160089@fpt.edu.vn>
2021-04-16 01:19:53 +0700

    16042021

--- BookManagement/Control/FileManipulation.java
+++ BookManagement/Control/FileManipulation.java
@@ -1,5 +1,64 @@
+import java.io.FileWriter;
+
 public class FileManipulation 
 
 {
+    private FileWriter fileWriter;
+    private BufferedWriter bufferedWriter;
+    private PrintWriter printWriter;
+    private Scanner scanner;
+
+    void openFiletoWrite(String filename)
+    {
+        try 
+        {
+            fileWriter = new FileWriter(filename, append = true);
+            bufferedWriter = new BufferedWriter(fileWriter);
+            printWriter = new PrintWriter(bufferedWriter);
+
+        } catch (Exception e) 
+            {
+            e.printStackTrace();
+            }
+        
+
+    }
+
+    void writeBooktoFile(Book book, String filname)
+    {
+        printWriter.println(book.getbookID() + "|" + book.getBookName() + "|" + book.getAuthor + "|" + book.getSpecializations
+                                                                         + "|" + book.getPublicYear + "|" + book.getQuantity);
     
+    
+    }
+
+    void writeReadertoFile(Reader reader, String filname)
+    {
+        printWriter.println(reader.getReaderID() + "|" + reader.getFullName() + "|" + reader.getAddress() + "|" + reader.getPhoneNumber());
+    
+    
+    }
+
+    void writeBookReaderManagementtoFile(BookReaderManagement brm, String filname)
+    {
+        printWriter.println(brm.getReaderID() + "|" + brm.getFullName()
+         + "|" + brm.getBookName() + "|" + brm.getBookID() + "|" + brm.getNumOfBorrow() + "|" + brm.getState());
+    
+    
+    }
+
+                                                                        
+    void closeFileAfterWrite(String filename)
+    {
+        try 
+        {
+            printWriter.close();
+            bufferedWriter.close();
+            fileWriter.close();    
+        } catch (Exception e) 
+            {
+            e.printStackTrace();
+            }
+    }
+
 }
